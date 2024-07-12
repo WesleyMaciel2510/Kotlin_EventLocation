2ab485ec7b90038bd90d8cf015ce23f94e42e879
Wesley Maciel <wesley.2510@hotmail.com>
2024-07-12 13:10:49 -0300

    CameraScreen Build and Open correctly

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -67,6 +67,17 @@ dependencies {
     implementation(libs.androidx.appcompat)
     implementation(libs.androidx.runtime.android)
     implementation("com.google.accompanist:accompanist-permissions:0.35.1-alpha")
+    // Jetpack Compose
+    implementation ("androidx.activity:activity-compose:1.9.0")
+    // CameraX
+    implementation ("androidx.camera:camera-camera2:1.3.4")
+    implementation ("androidx.camera:camera-lifecycle:1.0.2")
+    implementation ("androidx.camera:camera-view:1.0.0-alpha31")
+
+    // Zxing
+    implementation ("com.google.zxing:core:3.3.3")
+
+
     // new libs end block ==============
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
--- app/src/main/java/com/example/kotlin_portfolio/screens/camera/cameraScreen.kt
+++ app/src/main/java/com/example/kotlin_portfolio/screens/camera/cameraScreen.kt
@@ -1,44 +1,145 @@
 package com.example.kotlin_portfolio.screens.camera
 
-import androidx.compose.material3.Text
+import android.Manifest
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.activity.compose.BackHandler
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageAnalysis
+import androidx.camera.core.ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.camera.view.PreviewView
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalLifecycleOwner
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.viewinterop.AndroidView
+import androidx.core.content.ContextCompat
 import androidx.navigation.NavHostController
-import androidx.navigation.compose.rememberNavController
-import com.example.kotlin_portfolio.ui.theme.Kotlin_PortfolioTheme
+import com.example.kotlin_portfolio.MainActivity
 
 @Composable
 fun CameraScreen(modifier: Modifier = Modifier, navController: NavHostController) {
-    Text("CameraScreen")
-    /*val localContext = LocalContext.current
+    //Text("CameraScreen")
+    val context = LocalContext.current
     val lifecycleOwner = LocalLifecycleOwner.current
-    val cameraProviderFuture = remember {
-        ProcessCameraProvider.getInstance(localContext)
+    val cameraProviderFeature = remember {
+        ProcessCameraProvider.getInstance(context)
     }
-    AndroidView(
-        modifier = Modifier.fillMaxSize(),
-        factory = { context ->
-            val preview = Preview.Builder().build()
-            val cameraProvider = cameraProviderFuture.get()
+    var code by remember {
+        mutableStateOf("")
+    }
+    var hasReadCode by remember {
+        mutableStateOf(false)
+    }
+    var hasCamPermission by remember {
+        mutableStateOf(
+            ContextCompat.checkSelfPermission(
+                context,
+                Manifest.permission.CAMERA
+            ) == PackageManager.PERMISSION_GRANTED
+        )
+    }
 
-            preview.setSurfaceProvider(previewView.surfaceProvider)
+    // Request camera permission using ActivityResultContracts.RequestPermission
+    val launcher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.RequestPermission(),
+        onResult = { granted: Boolean -> hasCamPermission = granted }
+    )
 
-            runCatching {
-                cameraProvider.bindToLifecycle(
-                    lifecycleOwner,
-                    CameraSelector.DEFAULT_BACK_CAMERA,
-                    preview
+    // Launch permission request on initial composition
+    LaunchedEffect(key1 = true) {
+        launcher.launch(Manifest.permission.CAMERA)
+    }
+
+    // Composable content for the camera screen
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .background(Color(0xff40407a)),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        if (hasCamPermission) {
+            if (hasReadCode) {
+                LoadWebUrl(code)
+                BackHandler {
+                    restartApp(context)
+                }
+            } else {
+                AndroidView(
+                    factory = { context: Context ->
+                        val previewView = PreviewView(context)
+                        val preview = Preview.Builder().build()
+                        val selector = CameraSelector.Builder()
+                            .requireLensFacing(CameraSelector.LENS_FACING_BACK)
+                            .build()
+                        preview.setSurfaceProvider(previewView.surfaceProvider)
+                        val imageAnalysis = ImageAnalysis.Builder()
+                            .setBackpressureStrategy(STRATEGY_KEEP_ONLY_LATEST)
+                            .build()
+                        imageAnalysis.setAnalyzer(
+                            ContextCompat.getMainExecutor(context),
+                            QRCode { result ->
+                                code = result
+                                hasReadCode = true
+                            }
+                        )
+                        try {
+                            cameraProviderFeature.get().bindToLifecycle(
+                                lifecycleOwner,
+                                selector,
+                                preview,
+                                imageAnalysis
+                            )
+                        } catch (e: Exception) {
+                            e.printStackTrace()
+                        }
+                        previewView
+                    },
+                    modifier = Modifier.border(10.dp, color = Color(0xFFD6A2E8))
                 )
-            }.onFailure {
-                Log.e("CAMERA", "Camera bind error ${it.localizedMessage}", it)
             }
-            previewView
         }
-    )*/
+    }
 }
 
-@Preview(showBackground = true)
+@Composable
+fun LoadWebUrl(url: String) {
+    AndroidView(factory = {
+        WebView(it).apply {
+            webViewClient = WebViewClient()
+            loadUrl(url)
+        }
+    })
+}
+
+private fun restartApp(context: Context) {
+    val intent = Intent(context, MainActivity::class.java)
+    intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_NEW_TASK)
+    context.startActivity(intent)
+}
+
+/*
 @Composable
 fun PreviewCameraScreen() {
     val navController = rememberNavController()
@@ -46,3 +147,4 @@ fun PreviewCameraScreen() {
         CameraScreen(navController = navController)
     }
 }
+*/
